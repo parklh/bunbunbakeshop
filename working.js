35ba507cdcffdde3729d8f8f5336cee4fd6730a3
parklh <parklisah@gmail.com>
2017-10-20 18:57:41 -0400

    changing

--- working.js
+++ working.js
@@ -1,4 +1,4 @@
-/*if (window.location.href.match('cart.html') !=null) {
+if (window.location.href.match('cart.html') !=null) {
     localStorage.getItem('myOrder', JSON.stringify(order));
     return false;
 }
@@ -13,7 +13,6 @@ function addCart() {
   document.getElementById("quant").innerHTML = quant;
   localStorage.getItem('myOrder') 
 
-
 //QUANTITY IN CART
 //add local storage stuffs
 //REMOVE
@@ -22,9 +21,9 @@ function addCart() {
 
 
 //LIMIT CHECKBOXES
-var flavor = document.getElementsByName('flavor');
+var flavor = document.getElementsByName('flavors');
 var limit = 0;
-for (var i=0; i < flavor.length; i++){
+for (var i=0; i < flavors.length; i++){
 flavor[i].addEventListener('change', checkFlavor);
 }
 
@@ -44,24 +43,24 @@ var currentPrice = "$0.00";
 var pageFlavor = document.getElementById('pageFlavor').dataset.flavor;
 if (localStorage.getItem('myOrder')) {
   var order = JSON.parse(localStorage.getItem('myOrder'));
-  document.getElementById("quant").innerHTML = "Saved Price: " + order.price + " Amount: " + order.amount + ' Flavors: ' + order.flavors;
+  document.getElementById("yourOrder").innerHTML = "Saved Price: " + order.price + " Amount: " + order.amount + ' Flavors: ' + order.flavors;
 } else {
   var order = new Object();
-  document.getElementById("quant").innerHTML = "No Saved Order";
+  document.getElementById("yourOrder").innerHTML = "No Saved Order";
 }
 
 document.getElementById("add").addEventListener("click",addCart);
 function addCart() {
-  if (!document.getElementById('dropdown1').value===""){
+  if (!document.getElementById('dropdown').value===""){
     order.flavors = [pageFlavor];
     order.price = currentPrice;
-    order.amount = document.getElementById('dropdown1').value;
+    order.amount = document.getElementById('dropdown').value;
     for (var i=0; i < flavor.length; i++){
       if (flavor[i].checked) {
         order.flavors.push(flavor[i].value);
       }
     }
-    document.getElementById("quant").innerHTML = "Price: " + order.price + " Amount: " + order.amount + ' Flavors: ' + order.flavors;
+    document.getElementById("yourOrder").innerHTML = "Price: " + order.price + " Amount: " + order.amount + ' Flavors: ' + order.flavors;
     localStorage.setItem('myOrder', JSON.stringify(order)); //need a parameter here; for cart page
   }
 }
@@ -75,17 +74,14 @@ function addCart() {
 //  item.quantity -= 1;
 //  this.items.splice(this.items.indexOf(item), 1);
 //}
-*/
 
-//DROPDOWN2 TRIGGER
-//document.getElementById("dropdown2").style.display = "none";
-//document.getElementById("moreDescription").style.display = "none";
+
 
 function showFlavors() {
   if (document.getElementById('flavors').style.display === "none") {
     document.getElementById('flavors').style.display = "block";
     document.getElementById("moreDescription").style.display = "block";
-  document.getElementById('bun1').src='https://images.freshop.com/00024126012808/7cc1c32a2c77ca8958d0004bb0ad3322_medium.png';
+  document.getElementById('main').src='images/product2.png';
   }
 }
 
@@ -93,7 +89,7 @@ function hideFlavors() {
   if (document.getElementById('flavors').style.display === "block") {
     document.getElementById('flavors').style.display = "none";
     document.getElementById('moreDescription').style.display = "none";
-  document.getElementById('bun1').src='https://images.freshop.com/00221151000002/22fbd9158fed2d9ab85034c1fa06edcb_medium.png';
+  document.getElementById('main').src='images/product2.png';
   for (var i=0; i < flavor.length; i++){
   flavor[i].checked = false;
   }
